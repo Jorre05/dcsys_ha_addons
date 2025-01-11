c8755f07303ee1b0f22405061e8970ed47456bdb
Joris S <100357138+Jorre05@users.noreply.github.com>
2025-01-11 17:20:19 +0100

    Update github_checkout.sh

--- dcsys/rootfs/etc/cont-init.d/github_checkout.sh
+++ dcsys/rootfs/etc/cont-init.d/github_checkout.sh
@@ -6,8 +6,13 @@
 readonly GITHUB_PATH=/share
 declare github_repository
 
+if [ ! -d ${GITHUB_PATH}/dcsys_ha_config ]; then
+    bashio::log.info "DCSys configuratie is er niet, GIT checkout"
+    cd ${GITHUB_PATH}
+    git clone ${github_repository}
+fi
+
 github_repository=$(bashio::config 'github_repository')
 bashio::log.info  "GitHeb Checkout from ${github_repository} to ${GITHUB_PATH}"
-cd ${GITHUB_PATH}
-git clone ${github_repository}
+
 
