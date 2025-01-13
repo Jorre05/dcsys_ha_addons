224322631fc4166461d72e19ad3402dc9c0cbcca
Joris S <100357138+Jorre05@users.noreply.github.com>
2025-01-13 07:23:44 +0100

    Update github_checkout.sh

--- dns/rootfs/etc/cont-init.d/github_checkout.sh
+++ dns/rootfs/etc/cont-init.d/github_checkout.sh
@@ -5,8 +5,10 @@
 # ==============================================================================
 declare github_repository
 declare dcsys_config_root_dir
+declare listen_on
 
 github_repository=$(bashio::config 'github_repository')
+listen_on=$(bashio::config 'listen_on')
 dcsys_config_root_dir=$(bashio::config 'dcsys_config_root_dir')
 bashio::log.info  "GitHub Checkout from ${github_repository} to ${dcsys_config_root_dir}"
 
@@ -20,5 +22,6 @@ else
 fi
 
 cp -frp ${dcsys_config_root_dir}/dns/* /config
+sed "s/#LISTEN_ON#/${listen_on}/g" /config/named.conf -i
 chmod -R 755 /config/zones
 chown -R named:named /config/zones
