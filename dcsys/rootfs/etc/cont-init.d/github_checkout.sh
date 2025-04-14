46857aaf87ee05f35580cfd12df2ad597a2a3e33
Joris S <100357138+Jorre05@users.noreply.github.com>
2025-04-14 18:00:01 +0200

    Update github_checkout.sh

--- dcsys/rootfs/etc/cont-init.d/github_checkout.sh
+++ dcsys/rootfs/etc/cont-init.d/github_checkout.sh
@@ -10,15 +10,9 @@ github_repository=$(bashio::config 'github_repository')
 dcsys_config_root_dir=$(bashio::config 'dcsys_config_root_dir')
 bashio::log.info  "GitHub Checkout from ${github_repository} to ${dcsys_config_root_dir}"
 
-cd ${dcsys_config_root_dir}
-rm -fr *
-if [ ! -d ${dcsys_config_root_dir} ]; then
-    bashio::log.info "DCSys configuratie is er niet, GIT clone"
-    git clone ${github_repository} ${dcsys_config_root_dir}
-else
-    bashio::log.info "DCSys configuratie is er, GIT pull"
-    cd ${dcsys_config_root_dir}
-    git pull
-fi
+rm -fr ${dcsys_config_root_dir}
+mkdir -p ${dcsys_config_root_dir}
+bashio::log.info "DCSys configuratie is er niet, GIT clone"
+git clone ${github_repository} ${dcsys_config_root_dir}
 
 cp -frp ${dcsys_config_root_dir}/dcsys /etc
