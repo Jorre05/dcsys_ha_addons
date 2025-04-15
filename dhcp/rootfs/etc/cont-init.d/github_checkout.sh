e56cefcc6ca20a477a75a02f9de916365025117c
Joris S <100357138+Jorre05@users.noreply.github.com>
2025-04-15 14:06:03 +0200

    Update github_checkout.sh

--- dhcp/rootfs/etc/cont-init.d/github_checkout.sh
+++ dhcp/rootfs/etc/cont-init.d/github_checkout.sh
@@ -14,14 +14,10 @@ dns_servers=$(bashio::config 'dns_servers')
 dcsys_config_root_dir=$(bashio::config 'dcsys_config_root_dir')
 bashio::log.info  "GitHub Checkout from ${github_repository} to ${dcsys_config_root_dir}"
 
-if [ ! -d ${dcsys_config_root_dir} ]; then
-    bashio::log.info "DCSys configuratie is er niet, GIT clone"
-    git clone ${github_repository} ${dcsys_config_root_dir}
-else
-    bashio::log.info "DCSys configuratie is er, GIT pull"
-    cd ${dcsys_config_root_dir}
-    git pull
-fi
+rm -fr ${dcsys_config_root_dir}
+mkdir -p ${dcsys_config_root_dir}
+bashio::log.info "DCSys configuratie is er niet, GIT clone"
+git clone ${github_repository} ${dcsys_config_root_dir}
 
 cp -frp ${dcsys_config_root_dir}/dhcp/* /config
 sed "s/#LISTEN_ON#/${listen_on}/g" /config/kea-dhcp4.conf -i
