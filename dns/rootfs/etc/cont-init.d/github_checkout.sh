7fc2fc73cca2693f14d66110f0f56670c00ad78b
Joris S <100357138+Jorre05@users.noreply.github.com>
2025-04-15 14:05:28 +0200

    Update github_checkout.sh

--- dns/rootfs/etc/cont-init.d/github_checkout.sh
+++ dns/rootfs/etc/cont-init.d/github_checkout.sh
@@ -12,14 +12,10 @@ listen_on=$(bashio::config 'listen_on')
 dcsys_config_root_dir=$(bashio::config 'dcsys_config_root_dir')
 bashio::log.info  "GitHub Checkout from ${github_repository} to ${dcsys_config_root_dir}"
 
-if [ ! -d ${dcsys_config_root_dir} ]; then
-    bashio::log.info "DCSys configuratie is er niet, GIT clone"
-    git clone ${github_repository} ${dcsys_config_root_dir}
-else
-    bashio::log.info "DCSys configuratie is er, GIT pull"
-    cd ${dcsys_config_root_dir}
-    git pull
-fi
+rm -fr ${dcsys_config_root_dir}
+mkdir -p ${dcsys_config_root_dir}
+bashio::log.info "DCSys configuratie is er niet, GIT clone"
+git clone ${github_repository} ${dcsys_config_root_dir}
 
 cp -frp ${dcsys_config_root_dir}/dns/* /config
 sed "s/#LISTEN_ON#/${listen_on}/g" /config/named.conf -i
